04ad4eb8017b423d9aa59f699f17a879af518a8e
Bruno Hebling Vieira <bruno.hebling.vieira@usp.br>
2020-10-15 21:29:36 -0300

    Simplified publication listing

--- utils.jl
+++ utils.jl
@@ -68,41 +68,33 @@ Plug in the list of publications contained in the `/publications/` folder.
 function hfun_publications()
     curyear = year(Dates.today())
     io = IOBuffer()
-    for year in curyear:-1:2017
-        ys = "$year"
-        # year < curyear && write(io, "\n**$year**\n")
-        for month in 12:-1:1
-            ms = "0"^(month < 10) * "$month"
-            base = joinpath("publications", ys, ms)
-            isdir(base) || continue
-            posts = filter!(p -> endswith(p, ".md"), readdir(base))
-            days  = zeros(Int, length(posts))
-            lines = Vector{String}(undef, length(posts))
-            for (i, post) in enumerate(posts)
-                ps  = splitext(post)[1]
-                url = "/publications/$ys/$ms/$ps/"
-                surl = strip(url, '/')
-                title = pagevar(surl, :title)
-                doi = pagevar(surl, :doi)
-                date    = pagevar(surl, :rss_pubdate)
-                journal = pagevar(surl, :journal)
-                authors = pagevar(surl, :authors)
-                authors = replace(authors, "B.H. Vieira" => "**B.H. Vieira**")
-                if isnothing(date)
-                    date    = "$ys-$ms-01"
-                    days[i] = 1
-                else
-                    days[i] = day(date)
-                end
-                
-                plumx_badge = retrieve_plumx_badge(doi)
-                
-                lines[i] = publiline(plumx_badge, title, url, authors, journal, date)
-            end
-            # sort by day
-            foreach(line -> write(io, line), lines[sortperm(days, rev=true)])
+    # year < curyear && write(io, "\n**$year**\n")
+    posts = filter!(p -> endswith(p, ".md"), readdir("publications"))
+    days  = Vector{Date}(undef, length(posts))
+    lines = Vector{String}(undef, length(posts))
+    for (i, post) in enumerate(posts)
+        ps  = splitext(post)[1]
+        url = "/publications/$ps/"
+        surl = strip(url, '/')
+        title = pagevar(surl, :title)
+        doi = pagevar(surl, :doi)
+        date    = pagevar(surl, :rss_pubdate)
+        journal = pagevar(surl, :journal)
+        authors = pagevar(surl, :authors)
+        authors = replace(authors, "B.H. Vieira" => "**B.H. Vieira**")
+        if isnothing(date)
+            date    = ""
+            days[i] = 1
+        else
+            days[i] = date
         end
+        
+        plumx_badge = retrieve_plumx_badge(doi)
+        
+        lines[i] = publiline(plumx_badge, title, url, authors, journal, date)
     end
+    # sort by day
+    foreach(line -> write(io, line), lines[sortperm(days, rev=true)])
     # markdown conversion adds `<p>` beginning and end but
     # we want to  avoid this to avoid an empty separator
     r = Franklin.fd2html(String(take!(io)), internal=true, nop = true)
